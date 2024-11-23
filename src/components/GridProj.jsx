327b70542f56b27f292704c40c4ea370809d11d8
unknown <belgasmi.rayane1@gmail.com>
2024-11-23 21:22:16 +0100

    newnew

--- src/components/GridProj.jsx
+++ src/components/GridProj.jsx
@@ -4,7 +4,7 @@ import Cards from './Cards';
 
 export default function GridProj() {
   return (
-    <div className="grid md:grid-cols-3 p-4 w-[80%] gap-10 overflow-y-auto border  border-slate-700 shadow-inner shadow-black  md:max-h-[70vh] h-[55vh] scrollbar  scrollbar-thumb-[#F5004F] md:scrollbar-track-slate-700 bg-darkText dark:bg-darkText ">
+    <div className="grid md:grid-cols-3 p-4 w-[80%] md:gap-10 gap-5 overflow-y-auto border  border-slate-700 shadow-inner shadow-black  md:max-h-[70vh] h-[40vh] scrollbar  scrollbar-thumb-[#F5004F] md:scrollbar-track-slate-700 bg-darkText dark:bg-darkText ">
       {SLIDE.map((proj, index) => (
         <Cards infos={proj} key={index} />
       ))}
