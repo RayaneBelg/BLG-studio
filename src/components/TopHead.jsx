60ccabaafd1a83ba726a5a58989da06306b72505
unknown <belgasmi.rayane1@gmail.com>
2024-11-27 15:29:36 +0100

    okok

--- src/components/TopHead.jsx
+++ src/components/TopHead.jsx
@@ -4,12 +4,8 @@ import { FiMenu, FiX } from "react-icons/fi"; // Icons for Hamburger and Close
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-    const toggleMenu = () => {
-    setMenuOpen(!menuOpen);}
-  
-  
 
-  
+  const navLinks = ["Home", "About", "Services", "Contact"]; // Replace with your links
 
   return (
     <motion.div
@@ -24,20 +20,10 @@ const Navbar = () => {
         scale :0.9,
         transition: { duration: 0.3 }
       }}
-      onClick={toggleMenu}
       transition={{ delay: 1.5, duration: 0.8 }}
       className="z-50 fixed flex items-center justify-center text-lightText border-t-0 bg-rose h-[3vh] w-[10vh] left-[50%] transform translate-x-[-50%] border border-lightText dark:border-lightBg font-bold uppercase font-teko text-xl shadow-md shadow-black dark:shadow-lightBg"
     >
       <h1 className="">About</h1>
-      {
-        menuOpen && (
-          <div onClick={toggleMenu} className="fixed inset-0 z-40 bg-DarkBg dark:bg-lightBg flex items-center justify-center bg-opacity-50 dark:bg-opacity-50 ">
-            <div className=" bg-rose ">
-              <div><h1>ABOUT ME</h1></div>
-            </div>
-          </div>
-        )
-      }
     </motion.div>
   );
 };
