594adc766215a8e4e8b3b9163575162a9174149b
unknown <belgasmi.rayane1@gmail.com>
2024-12-01 15:35:48 +0100

    og

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -4,7 +4,7 @@ import { IoMoonOutline } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
 import { MdSunny } from "react-icons/md";
 import { IoMdMoon } from "react-icons/io";
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function DarkModeToggle() {
   const { darkMode, setDarkMode } = useContext(DarkModeContext);
@@ -14,11 +14,12 @@ export default function DarkModeToggle() {
     whileHover={{ scale: 1.075 }} 
     whileTap={{scale : 0.9}}
     initial={{opacity : 0.1}}
-    animate
+    animate={{opacity : 1}}
+    transition={{duration : 0.8 , delay : 2.5}}
       onClick={() => setDarkMode(!darkMode)}
       className=" absolute left-[50%] transform translate-x-[-50%] bottom-1 z-30"
     >
-      {darkMode ? <MdSunny size={40} className='  text-rose  bg-lightText  rounded-full  shadow-[5px_5px_10px_#141a22,_-5px_-5px_10px_#526888] ' /> : 
+      {darkMode ? <AnimatePresence> <MdSunny size={40} className='  text-rose  bg-lightText  rounded-full  shadow-[5px_5px_10px_#141a22,_-5px_-5px_10px_#526888] ' /> </AnimatePresence> : 
       <IoMdMoon  size={40} className='text-lightText z-30  shadow-[5px_5px_10px_#717575,-5px_-5px_10px_#ffffff] rounded-full  bg-lightbg'/>
       }
     </motion.button>
