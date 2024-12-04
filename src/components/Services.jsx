39a5ba87b8b0656fe5bb04e9f843922e59a7b791
unknown <belgasmi.rayane1@gmail.com>
2024-12-04 18:56:09 +0100

    jj

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,35 +3,42 @@ import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeli
 import 'react-vertical-timeline-component/style.min.css';
 import { MdOutlineWork } from "react-icons/md";
 import {SERVICES } from '../assets/aboutdata';
-import { color } from 'framer-motion';
+import { useContext } from 'react';
+
+import { DarkModeContext } from './DarkModeProvider';
 
 export default function Services() {
+
+  
+  const { darkMode } = useContext(DarkModeContext);
+
+
   return (
-    <div className='flex flex-col items-center w-full'>
-         {
-            SERVICES.map((item , id)=>(
-                <VerticalTimeline
-                lineColor={'#aae114'}
-                layout=''
-                className=''>
+    <div className=' flex flex-col items-center w-full'>
+    {
+        SERVICES.map((item , id)=>(
+            <VerticalTimeline
+            lineColor={darkMode ? '#d1d8d8' : "#334155"}
+            layout=''
+            className=''>
 
 <VerticalTimelineElement
-    className=" font-teko  ] "
-    contentStyle={{ background: '#334155', color: '#aae114', border : "2px solid #aae114" , borderRadius : '0' }}
-    contentArrowStyle={{ borderRight: '10px solid  #aae114' }}
-    
-    date="2011 - present"
-    iconStyle={{ background: '#aae114',  }}
-    icon={<MdOutlineWork color='#334155' />}
-  >
-    <h3 className=" text-xl font-bold">{item.name}</h3>
-    <h4 className="">{item.company}</h4>
-    <p>
-      Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-    </p>
-  </VerticalTimelineElement>
-  
-                </VerticalTimeline>
+className=" font-teko "
+contentStyle={{ background: darkMode ?  "#334155" : '#d1d8d8' , color: darkMode ?  '#aae114' : "#334155"  , border : darkMode ? "2px solid #d1d8d8 " : "2px solid #334155" , borderRadius : '0' }}
+contentArrowStyle={{ borderRight: '10px solid  #aae114' }}
+
+date={item.date}
+iconStyle={{ background: '#aae114',  }}
+icon={<MdOutlineWork color='#334155' />}
+>
+<h3 className=" text-2xl dark:text-darkText font-bold">{item.name}</h3>
+<h4 className="text-xl">{item.desc}</h4>
+<p className='dark:text-darkText'>
+  Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+</p>
+</VerticalTimelineElement>
+
+            </VerticalTimeline>
 
 
             ))
