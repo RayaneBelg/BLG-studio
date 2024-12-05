e4f13856e4f9cac64283406dde496b7926ce84b5
unknown <belgasmi.rayane1@gmail.com>
2024-12-05 01:39:34 +0100

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
    
 
-   <div className="h-dvh overflow-hidden p-2  dark:bg-DarkBg bg-lightBg  ">
+   <div className="h-dvh overflow-hidden p-2  dark:bg-DarkBg bg-lightBg transition-all duration-700 ">
 
    
 
